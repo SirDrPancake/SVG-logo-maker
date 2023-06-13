280e8daf441aa7b5fa7074bf0b39031b7b799418
joel Quezada <halo.joel.quezada@gmail.com>
2023-06-12 21:23:09 -0700

    changes

--- Develop/index.js
+++ Develop/index.js
@@ -35,8 +35,8 @@ function generateLogo(answers) {
   const { text, textColor, shape, shapeColor } = answers;
 
   const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-                        <rect width="100%" height="100%" fill="${shapeColor}" />
-                        <text x="50%" y="50%" fill="${textColor}" font-size="60" text-anchor="middle">${text}</text>
+                      <rect width="100%" height="100%" fill="${shapeColor}" />
+                    <text x="50%" y="50%" fill="${textColor}" font-size="60" text-anchor="middle">${text}</text>
                       </svg>`;
 
   fs.writeFile('logo.svg', svgContent, (err) => {
