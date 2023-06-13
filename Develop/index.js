f40dbf88daa77912ca992a517fa86c852ef40b42
joel Quezada <halo.joel.quezada@gmail.com>
2023-06-12 20:58:43 -0700

    question prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,1 +1,18 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'text',
+    message: 'Enter the text for the logo (up to three characters):',
+    validate: (input) => {
+      if (input.length > 3) {
+        return 'Please enter up to three characters.';
+      }
+      return true;
+    }
+  },
+  
+];
+
